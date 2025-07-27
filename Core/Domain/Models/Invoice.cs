b3517a5233610b9dc317e2515cd10353b76d156e
SOHA <SOHA@LAPTOP-D0C015FV>
2025-07-26 20:26:48 -0800

    #SettingUpTheProjectAndDefiningTheModels

---
+++ Core/Domain/Models/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Invoice : BaseEntity<int>
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ Core/Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User : BaseEntity<int>
+    {
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } 
+    }
+}
